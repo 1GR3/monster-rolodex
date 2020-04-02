c368aea8ad975bec8f2b823dd075054d76f03431
Ivo Gregurec <ivo.gregurec@gmail.com>
2020-04-02 11:31:21 +0200

    Updates

---
+++ precache-manifest.97e44aefd9c54c4f4e837872940fdfcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1477e60bdb38b9106295628a38b40be9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b822c15b74feb784441a",
+    "url": "/monsters-rolodex/static/css/main.c1ebfb5b.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b822c15b74feb784441a",
+    "url": "/monsters-rolodex/static/js/main.9a751e93.chunk.js"
+  },
+  {
+    "revision": "96e4dc8f64b3356b837b",
+    "url": "/monsters-rolodex/static/js/runtime-main.6b89a6eb.js"
+  }
+]);
